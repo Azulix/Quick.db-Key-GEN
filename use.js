affc09dbf993a58db3c6ba1fa573425fa8d7a410
Xenos <74306156+Azulix@users.noreply.github.com>
2021-07-18 03:51:40 +0200

    Add files via upload

---
+++ use.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+config = require('../../config.json')
+const db = require("quick.db")
+const code = require('@codedipper/random-code')
+
+module.exports = {
+
+    run: async (client, message, args) => {
+
+        let prem = db.get(`premium_subs${args[0]}`, client.user.id)
+        if(prem === null){
+            message.channel.send('Invalide')
+        }else message.channel.send('Valide')
+		db.delete(`premium_subs${args[0]}`)
+		console.log(`La clé ${args[0]} a été utlisé avec succès`)
+        
+      
+
+
+    },
+
+
+
+    name: 'use'
+
+}
+
+
